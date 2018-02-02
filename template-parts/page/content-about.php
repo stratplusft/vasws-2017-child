83fabcb5379fbdd8da674d66a26a3dd82c0144ee
Bill D <bill@typhina.co.uk>
2018-02-02 23:19:58 +0000

    contact page code

--- template-parts/page/content-about.php
+++ template-parts/page/content-about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying page content in page.php
+ * Template part for displaying about us page content in page.php
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -19,13 +19,38 @@
 	<div class="entry-content">
 		<?php the_content(); ?>
                 <?php 
-                    if(get_field('town_link')){
-                        $image = get_field('town_image');
+                    if(get_field('photo-1')){
+                        $image = get_field('photo-1');
                         echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
-                        echo '<div class="panel-image" style="background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
+                        echo '<div class="panel-contact-image" style="background-repeat:no-repeat; background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
                         echo '</div>';
                         echo '<div class="entry-panel-header">'; 
-                        echo '<h4>' . get_field('town_link') . '</h4>';
+                        echo '<h4>' . get_field('name-1') . '</h4>';
+                         echo '<p>' . get_field('role-1') . '</p>';
+                        echo '<p>' . get_field('e-mail-1') . '</p>';
+                        echo '<p>' . get_field('telephone-1') . '</p>';
+                       
+                        ?>
+                       
+                 <?php
+                        echo '</div>';
+                        echo '</article>';
+                        
+                    }
+                
+                ?>
+            <?php 
+                    if(get_field('photo-2')){
+                        $image = get_field('photo-2');
+                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
+                        echo '<div class="panel-contact-image" style="background-repeat:no-repeat; background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
+                        echo '</div>';
+                        echo '<div class="entry-panel-header">'; 
+                        echo '<h4>' . get_field('name-2') . '</h4>';
+                         echo '<p>' . get_field('role-2') . '</p>';
+                        echo '<p>' . get_field('e-mail-2') . '</p>';
+                        echo '<p>' . get_field('telephone-2') . '</p>';
+                       
                         ?>
                        
                  <?php
@@ -36,91 +61,7 @@
                 
                 ?>
             
-                <?php 
-                    if(get_field('town_link_2')){
-                        $image = get_field('town_image_2');
-                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
-                        echo '<div class="panel-image" style="background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
-                        echo '</div>';
-                        echo '<div class="entry-panel-header">'; 
-                        echo '<h4>' . get_field('town_link_2') . '</h4>';
-                        ?>
-                       
-                 <?php
-                        echo '</div>';
-                        echo '</article>';
-                        
-                    }
                 
-                ?>
-            <?php 
-                    if(get_field('town_link_3')){
-                        $image = get_field('town_image_3');
-                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
-                        echo '<div class="panel-image" style="background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
-                        echo '</div>';
-                        echo '<div class="entry-panel-header">'; 
-                        echo '<h4>' . get_field('town_link_3') . '</h4>';
-                        ?>
-                       
-                 <?php
-                        echo '</div>';
-                        echo '</article>';
-                        
-                    }
-                
-                ?>
-            <?php 
-                    if(get_field('town_link_4')){
-                        $image = get_field('town_image_4');
-                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
-                        echo '<div class="panel-image" style="background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
-                        echo '</div>';
-                        echo '<div class="entry-panel-header">'; 
-                        echo '<h4>' . get_field('town_link_4') . '</h4>';
-                        ?>
-                       
-                 <?php
-                        echo '</div>';
-                        echo '</article>';
-                        
-                    }
-                
-                ?>
-            <?php 
-                    if(get_field('town_link_5')){
-                        $image = get_field('town_image_5');
-                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
-                        echo '<div class="panel-image" style="background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
-                        echo '</div>';
-                        echo '<div class="entry-panel-header">'; 
-                        echo '<h4>' . get_field('town_link_5') . '</h4>';
-                        ?>
-                       
-                 <?php
-                        echo '</div>';
-                        echo '</article>';
-                        
-                    }
-                
-                ?>
-            <?php 
-                    if(get_field('town_link_6')){
-                        $image = get_field('town_image_6');
-                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
-                        echo '<div class="panel-image" style="background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
-                        echo '</div>';
-                        echo '<div class="entry-panel-header">'; 
-                        echo '<h4>' . get_field('town_link_6') . '</h4>';
-                        ?>
-                       
-                 <?php
-                        echo '</div>';
-                        echo '</article>';
-                        
-                    }
-                
-                ?>
 
 		<?php	wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
