086a3ceb4b3c120eff7aad968b469503085f62c2
Bill D <bill@typhina.co.uk>
2018-02-03 15:33:31 +0000

    update styles and template for contact page

--- template-parts/page/content-about.php
+++ template-parts/page/content-about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying about us page content in page.php
+ * Template part for displaying about us contact page content in page.php
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -18,6 +18,7 @@
 	</header><!-- .entry-header -->
 	<div class="entry-content">
 		<?php the_content(); ?>
+            <div class="flex-row-panel">
                 <?php 
                     if(get_field('photo-1')){
                         $image = get_field('photo-1');
@@ -60,6 +61,118 @@
                     }
                 
                 ?>
+            
+            <?php 
+                    if(get_field('photo-3')){
+                        $image = get_field('photo-3');
+                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
+                        echo '<div class="panel-contact-image" style="background-repeat:no-repeat; background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
+                        echo '</div>';
+                        echo '<div class="entry-panel-header">'; 
+                        echo '<h4>' . get_field('name-3') . '</h4>';
+                         echo '<p>' . get_field('role-3') . '</p>';
+                        echo '<p>' . get_field('email-3') . '</p>';
+                        echo '<p>' . get_field('telephone-3') . '</p>';
+                       
+                        ?>
+                       
+                 <?php
+                        echo '</div>';
+                        echo '</article>';
+                        
+                    }
+                
+                ?>
+            
+            <?php 
+                    if(get_field('photo-4')){
+                        $image = get_field('photo-4');
+                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
+                        echo '<div class="panel-contact-image" style="background-repeat:no-repeat; background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
+                        echo '</div>';
+                        echo '<div class="entry-panel-header">'; 
+                        echo '<h4>' . get_field('name-4') . '</h4>';
+                         echo '<p>' . get_field('role-4') . '</p>';
+                        echo '<p>' . get_field('email-4') . '</p>';
+                        echo '<p>' . get_field('telephone-4') . '</p>';
+                       
+                        ?>
+                       
+                 <?php
+                        echo '</div>';
+                        echo '</article>';
+                        
+                    }
+                
+                ?>
+            
+            <?php 
+                    if(get_field('photo-5')){
+                        $image = get_field('photo-5');
+                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
+                        echo '<div class="panel-contact-image" style="background-repeat:no-repeat; background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
+                        echo '</div>';
+                        echo '<div class="entry-panel-header">'; 
+                        echo '<h4>' . get_field('name-5') . '</h4>';
+                         echo '<p>' . get_field('role-5') . '</p>';
+                        echo '<p>' . get_field('email-5') . '</p>';
+                        echo '<p>' . get_field('telephone-5') . '</p>';
+                       
+                        ?>
+                       
+                 <?php
+                        echo '</div>';
+                        echo '</article>';
+                        
+                    }
+                
+                ?>
+            
+            <?php 
+                    if(get_field('photo-6')){
+                        $image = get_field('photo-6');
+                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
+                        echo '<div class="panel-contact-image" style="background-repeat:no-repeat; background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
+                        echo '</div>';
+                        echo '<div class="entry-panel-header">'; 
+                        echo '<h4>' . get_field('name-6') . '</h4>';
+                         echo '<p>' . get_field('role-6') . '</p>';
+                        echo '<p>' . get_field('email-6') . '</p>';
+                        echo '<p>' . get_field('telephone-6') . '</p>';
+                       
+                        ?>
+                       
+                 <?php
+                        echo '</div>';
+                        echo '</article>';
+                        
+                    }
+                
+                ?>
+            
+            <?php 
+                    if(get_field('photo-7')){
+                        $image = get_field('photo-7');
+                        echo '<article class="twentyseventeen-panel  page type-page status-publish has-post-thumbnail hentry">';
+                        echo '<div class="panel-contact-image" style="background-repeat:no-repeat; background-image: url( '?><?php echo $image['url']?><?php echo ' ">';
+                        echo '</div>';
+                        echo '<div class="entry-panel-header">'; 
+                        echo '<h4>' . get_field('name-7') . '</h4>';
+                         echo '<p>' . get_field('role-7') . '</p>';
+                        echo '<p>' . get_field('email-7') . '</p>';
+                        echo '<p>' . get_field('telephone-7') . '</p>';
+                       
+                        ?>
+                       
+                 <?php
+                        echo '</div>';
+                        echo '</article>';
+                        
+                    }
+                
+                ?>
+                
+            </div> <!-- end flex-row-panel -->
             
                 
 
